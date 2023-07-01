f88256c7e9e345ae2c10a7226de5c19a6be67c54
rafaelalessandro <noprafa@gmail.com>
2023-07-01 12:56:12 -0300

    add(printing study)

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -8,5 +8,49 @@
 // I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    let a = 2;
+    println!("Hello {}!", "World2");
+
+
+    println!("Hello {} {}!", "World2", "Teste");
+
+    
+    // Positional arguments
+    println!("string sample -> {0}", a);
+    // Named args
+    println!("string sample -> {frango}", frango=a);
+
+    eprintln!("Exemplo de erro!!");
+
+    // You can right-justify text with a specified width. This will
+    // output "    1". (Four white spaces and a "1", for a total width of 5.)
+    println!("{number:>5}", number=1);
+
+    //variable:> means that empty space will bet put on variable left side
+    //variable:0<5 means that 0 will be put on variable right side
+
+    // You can pad numbers with extra zeroes,
+    // and left-adjust by flipping the sign. This will output "10000".
+    println!("{number:0<5}", number=1);
+
+    // You can use named arguments in the format specifier by appending a `$`.
+    println!("{number:3>width$}", number=1, width=5);
+
+    // Only types that implement fmt::Display can be formatted with `{}`. User-
+    // defined types do not implement fmt::Display by default.
+
+    #[allow(dead_code)] // disable `dead_code` which warn against unused module
+    struct Structure(i32);
+
+    // This will not compile because `Structure` does not implement
+    // fmt::Display.
+    // println!("This struct `{}` won't print...", Structure(3));
+    // TODO ^ Try uncommenting this line
+
+    // For Rust 1.58 and above, you can directly capture the argument from a
+    // surrounding variable. Just like the above, this will output
+    // "    1", 4 white spaces and a "1".
+    let number: f64 = 1.0;
+    let width: usize = 5;
+    println!("{number:>width$}");
 }
